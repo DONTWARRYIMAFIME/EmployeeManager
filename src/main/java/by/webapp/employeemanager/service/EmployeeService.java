d25acec5bf95257d8b13d96e88d3ea3c845b5d02
Kostyukovich_V_A <demon4ik228228@gmail.com>
2021-07-12 13:56:27 +0300

    Created employee repository with first method (addEmployee)

---
+++ src/main/java/by/webapp/employeemanager/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package by.webapp.employeemanager.service;
+
+import by.webapp.employeemanager.model.Employee;
+import by.webapp.employeemanager.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepository.save(employee);
+    }
+}
